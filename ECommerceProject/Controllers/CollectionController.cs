385a56197686f1182322a87a099a259dc47c88a9
wassimAI - Wassim Ibrahim <40058827+WassimInstaclass@users.noreply.github.com>
2019-08-05 19:19:27 +0400

    Fixed Modal - started with all pages - done with collections page, next is add some stuff to collections and more pages

---
+++ ECommerceProject/Controllers/CollectionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECommerceProject.Models;
+using ECommerceProject.Models.MainVM;
+using ECommerceProject.Models.ViewModels;
+
+namespace ECommerceProject.Controllers
+{
+    public class CollectionController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Collection
+        [ActionName("all-collections")]
+        public ActionResult AllCollections()
+        {
+            var listOfCollections = db.collections.ToArray().Select(x => new CollectionVM(x)).ToList();
+
+            CollectionsPageVM model = new CollectionsPageVM
+            {
+                Collections = listOfCollections
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ ECommerceProject/Models/MainVM/CollectionsPageVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ECommerceProject.Models.ViewModels;
+
+namespace ECommerceProject.Models.MainVM
+{
+    public class CollectionsPageVM
+    {
+        public IEnumerable<CollectionVM> Collections { set; get; }
+    }
+}
